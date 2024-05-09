3991ca9633075cca4c27749ca7b59bd8ec6af175
ChristopherLit <litchristopher@gmail.com>
2024-05-09 03:11:14 -0400

    Added header formats

--- src/components/Headder.jsx
+++ src/components/Headder.jsx
@@ -1,21 +1,15 @@
 import { Christopher } from "../assets";
 
 const Headder = () => {
-  return <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
-    <div className="flex items-center px-5 lg:px-7.5 xl:px-10 py-7 max-lg:py-4">
-
-    <a className="block w-[12rem] xl:mr-8" href="#hero"> 
-        <img src={Christopher} width={190} height={40} alt="Christopher" />
-    
-        
-    </a>
-
-    
+  return (
+    <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 py-7 max-lg:py-4">
+        <a className="block w-[12rem] xl:mr-8" href="#hero">
+          <img src={Christopher} width={190} height={40} alt="Christopher" />
+        </a>
+      </div>
     </div>
-    
-
-  
-  </div>;
+  );
 };
 
 export default Headder;
