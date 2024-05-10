ecb17a992df571a83ce749d6f9e01660770309d0
ChristopherLit <litchristopher@gmail.com>
2024-05-10 18:41:07 -0400

    Changed spacing under hero

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import ButtonGradient from './assets/svg/ButtonGradient';
 import Button from './components/Button';
 import Headder from './components/Headder';
 import Hero from './components/Hero';
+import Projects from './components/Projects';
 
 const App = () => {
 
@@ -10,6 +11,7 @@ const App = () => {
     <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
       <Headder />
       <Hero />
+      <Projects />
     </div>
    <ButtonGradient />
    </>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,6 @@ const RedoAnimText = () => {
     "web app developer",
     "mobile app developer",
     "cat enthusiast (and dogs)",
-    
   ];
 
   const baseText = useTransform(textIndex, (latest) => texts[latest] || "");
@@ -49,32 +48,33 @@ const RedoAnimText = () => {
           }
           updatedThisRound.set(true);
         }
-      }
+      },
     });
-  
   }, []);
 
-  return (
-    <motion.span className="inline">
-      {displayText} 
-    </motion.span>
-  );
+  return <motion.span className="inline">{displayText}</motion.span>;
 };
 
 const Hero = () => {
   const parallaxRef = useRef(null);
-    return (
-      <Section
-        className="pt-[12rem] -mt-[5.25rem] flex justify-center"
-        crosses
-        crossesOffset="lg:translate-y-[5.25rem]"
-        customPaddings
-        id="hero"
-        style={{ position: 'relative' }}
-      >
-        <div className="container relative" ref={parallaxRef}>
-          <div className="relative z-1 max-w-[73rem] mx-auto text-center mb-[14rem] md:mb-25 lg:mb-[13rem]" style={{ textAlign: 'center' }}> {/* Centering the text */}
-            <h1 className="h1 mb-6">Hi, I'm{" "}
+  return (
+    <Section
+      className="pt-[12rem] -mt-[5.25rem] flex justify-center"
+      crosses
+      crossesOffset="lg:translate-y-[5.25rem]"
+      customPaddings
+      id="hero"
+      style={{ position: "relative" }}
+    >
+      <div className="container relative" ref={parallaxRef}>
+        <div
+          className="relative z-1 max-w-[73rem] mx-auto text-center mb-[14rem] md:mb-25 lg:mb-[13rem]"
+          style={{ textAlign: "center" }}
+        >
+          {" "}
+          {/* Centering the text */}
+          <h1 className="h1 mb-6">
+            Hi, I'm{" "}
             <span className="inline-block relative">
               Christopher{" "}
               <img
@@ -85,19 +85,22 @@ const Hero = () => {
                 alt="Curve"
               />
             </span>
-            
-            </h1>
-            <h1 className="h1 mb-6" style={{ position: 'absolute', left: '17%', textAlign: 'left' }}> {}
-              I'm a{" "}
-              <RedoAnimText></RedoAnimText>
-            </h1>
-          </div>
-          <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
+          </h1>
+          <h1
+            className="h1 mb-6"
+            style={{ position: "absolute", left: "17%", textAlign: "left" }}
+          >
+            {" "}
+            {}
+            I'm a <RedoAnimText></RedoAnimText>
+          </h1>
+        </div>
+        <div className="relative max-w-[23rem] h-[60rem] mx-auto md:max-w-5xl xl:mb-24">
           <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
             <div className="relative bg-n-8 rounded-[1rem]">
-              <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]"/>
+              <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
               <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490] flex justify-center items-center">
-              <img
+                <img
                   src={robot}
                   className="w-full"
                   width={1024}
@@ -105,57 +108,49 @@ const Hero = () => {
                   alt="AI"
                 />
 
-                <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2"/>
-
-               
+                <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2" />
 
                 <ScrollParallax isAbsolutelyPositioned>
                   <Notification
-                  className="hidden absolute -right-[5.5rem] bottom-[18rem] w-[15rem] xl:flex"
-                  title="Badminton"
-                  imageName="image-1.png"
+                    className="hidden absolute -right-[5.5rem] bottom-[18rem] w-[15rem] xl:flex"
+                    title="Badminton"
+                    imageName="image-1.png"
                   />
 
                   <Notification
-                  className="hidden absolute -left-[5.5rem] bottom-[7.5rem] w-[15rem] xl:flex"
-                  title="Chess"
-                  imageName="image-2.png"
+                    className="hidden absolute -left-[5.5rem] bottom-[7.5rem] w-[15rem] xl:flex"
+                    title="Chess"
+                    imageName="image-2.png"
                   />
 
                   <Notification
-                  className="hidden absolute -right-[5.5rem] bottom-[2rem] w-[15rem] xl:flex"
-                  title="Gym"
-                  imageName="image-3.png"
+                    className="hidden absolute -right-[5.5rem] bottom-[2rem] w-[15rem] xl:flex"
+                    title="Gym"
+                    imageName="image-3.png"
                   />
-
-
                 </ScrollParallax>
-
               </div>
-              </div>
+            </div>
 
-                <Gradient />
-              </div>
+            <Gradient />
+          </div>
 
-              <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
-                <img src={gradientHero}
-                  className="w-full"
-                  width={1440}
-                  height={1880}
-                  alt="hero"
-                  >
-                </img>
+          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[65%]">
+            <img
+              src={gradientHero}
+              className="w-full"
+              width={1440}
+              height={1880}
+              alt="hero"
+            ></img>
+          </div>
 
-
-              </div>
-              <BackgroundCircles />
-              </div>
-
+          <BackgroundCircles />
         </div>
-        <BottomLine />
-      </Section>
-    );
-  };
-  
-  export default Hero;
-  
+      </div>
+      <BottomLine />
+    </Section>
+  );
+};
+
+export default Hero;
