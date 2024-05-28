f0199a30d341dd032554866f4742c25037bc547a
ChristopherLit <litchristopher@gmail.com>
2024-05-28 03:51:46 -0400

    Description section

---
+++ components/Description.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TextReveal from './ui/text-reveal'
+
+const Description = () => {
+  return (
+    <div className="z-10 flex min-h-[16rem] items-center justify-center rounded-lg border bg-white dark:bg-black">
+      <TextReveal text="Magic UI will change the way you design." />
+    </div>
+  )
+}
+
+export default Description
