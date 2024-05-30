8e45761e7cee67feb751eab415cfdd2c271a1f9a
ChristopherLit <litchristopher@gmail.com>
2024-05-30 01:03:01 -0400

    Added Projects section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Description from "@/components/Description";
 import Grid from "@/components/Grid";
+import Headder from "@/components/Header";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
@@ -9,7 +10,7 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[{name: 'Home', link: '/', icon: <FaHome />}]}/>
+        <Headder />
         <Hero />
         <Grid />
         <Description />
